c64511dc08bf91360bf60d778f63f15a5a63c253
Sam Anklesaria <amsay@amsay.net>
2015-07-22 23:36:59 -0500

    List up and down

--- Lists.py
+++ Lists.py
@@ -1,70 +1,54 @@
 import sublime, sublime_plugin, re
 
+# makes corrections, starting the line after 'line' with x+1
+def correct(self, edit, line, indent, x, before, after):
+    editrow, _ = self.view.rowcol(line.end())
+    while True:
+        editrow += 1
+        x += 1
+        newline = self.view.line(self.view.text_point(editrow, 0))
+        if newline.a == line.a or not self.view.substr(newline).startswith(indent) or newline.empty():
+            return
+        line = newline
+        m = re.match("^" + indent + "[^\s\d]*\d+[^\s]* +(.*)$", self.view.substr(newline))
+        if not m:
+            continue
+        self.view.replace(edit, newline, before + str(x) + after + m.group(1))
+
+# returns indent of parent, current line, x of parent, before of parent, after of parent
+def findParent(self):
+    line = self.view.line(self.view.sel()[0])
+    m = re.match(r"^(\s*)", self.view.substr(line))
+    indent = m.group(1)
+    row, _ = self.view.rowcol(line.end())
+    prevline = line
+    while True:
+        newline = self.view.line(self.view.text_point(row - 1, 0))
+        if newline == prevline or not self.view.substr(newline).startswith(indent):
+            return indent, line, "", -1, ""
+        prevline = newline
+        row -= 1
+        m = re.match("^" + indent + "([^\s\d]*)(\d+)([^\s]* +)", self.view.substr(newline))
+        if m:
+            return indent, line, m.group(1), int(m.group(2)) + 1, m.group(3)
+        m = re.match("^" + indent + "([^\s\d]+ +)", self.view.substr(newline))
+        if m:
+            return indent, line, m.group(1), -1, ""
+
 class ListContinueCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        line = self.view.line(self.view.sel()[0])
-        m = re.match(r"^(\s*)", self.view.substr(line))
-        indent = m.group(1)
-        row, _ = self.view.rowcol(line.end())
-        prevline = line
-        while True:
-            newline = self.view.line(self.view.text_point(row - 1, 0))
-            if newline == prevline or not self.view.substr(newline).startswith(indent):
-                return
-            prevline = newline
-            row -= 1
+        indent, line, before, x, after = findParent(self)
+        if x < 0:
+            self.view.insert(edit, line.end(), before)
+        else:
+            self.view.insert(edit, line.end(), before + str(x) + after)
+            correct(self, edit, self.view.line(line), indent, x, before, after)
 
-            # Test for numbered bullet
-            m = re.match("^" + indent + "([^\s\d]*)(\d+)([^\s]* +)", self.view.substr(newline))
-            if m:
-                self.view.insert(edit, line.end(), m.group(1) + str(int(m.group(2)) + 1) + m.group(3))
-                line = self.view.line(line)
-                editrow, _ = self.view.rowcol(line.end())
-                while True:
-                    editrow += 1
-                    newline = self.view.line(self.view.text_point(editrow, 0))
-                    if newline.a == line.a or not self.view.substr(newline).startswith(indent) or newline.empty():
-                        return
-                    line = newline
-                    m = re.match("^(" + indent + "[^\s\d]*)(\d+)(.*)$", self.view.substr(newline))
-                    if not m:
-                        continue
-                    self.view.replace(edit, newline, m.group(1) + str(int(m.group(2)) + 1) + m.group(3))
-                return
-
-            # Test for static bullet
-            m = re.match("^(" + indent + "[^\s]* +)", self.view.substr(newline))
-            if m:
-                self.view.insert(edit, line.end(), m.group(1))
-                return
-
-# class ListUpCommand(sublime_plugin.TextCommand):
-#   def run(self, edit):
-
-# class ListDownCommand(sublime_plugin.TextCommand):
-#   def run(self, edit):
-
-class SelectIndentCommand(sublime_plugin.TextCommand):
+class ListFixCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        line = self.view.line(self.view.sel()[0])
-        m = re.match(r"^(\s*)", self.view.substr(line))
-        indent = m.group(1)
+        indent, line, before, x, after = findParent(self)
         row, _ = self.view.rowcol(line.end())
-        uprow = row
-        downrow = row
-        while True:
-            newline = self.view.line(self.view.text_point(uprow - 1, 0))
-            if newline == line or not self.view.substr(newline).startswith(indent):
-                break
-            line = newline
-            uprow -= 1
-        while True:
-            newline = self.view.line(self.view.text_point(downrow + 1, 0))
-            if newline == line or not self.view.substr(newline).startswith(indent):
-                break
-            line = newline
-            downrow += 1
-        self.view.sel().add(
-            self.view.line(sublime.Region(
-            self.view.text_point(uprow, 0),
-            self.view.text_point(downrow, 0))))
+        newline = self.view.line(self.view.text_point(row - 1, 0))
+        correct(self, edit, newline, indent, x-1, before, after)
+
+
